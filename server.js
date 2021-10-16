479bf69df517f8d33bd80356380efa978759d965
Banjac, Igor <igor.banjac@yahoo.com>
2021-10-16 11:21:25 +0200

    added colors package for dev

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
 const app = require("./app")
+const colors = require("colors")
 const port = process.env.PORT || 3000
 
-app.listen(port)
+app.listen(
+  port,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold
+  )
+)
